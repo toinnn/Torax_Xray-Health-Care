7c3400db6b6823158c08447f018b0669fa063cb4
toinnn <limaalyson@hotmail.com>
2023-10-02 16:13:46 -0300

    Update Transformer_Decoder.py
    Aparentemente o decoder já recebeu todos os ajustes que estava precisando

--- Transformer_Decoder.py
+++ Transformer_Decoder.py
@@ -6,7 +6,7 @@ import torch.nn.utils.prune as prune
 import matplotlib.pyplot as plt
 import random as rd
 from torch.autograd import Variable
-import heapq
+# import heapq
 # from skip_Gram import skip_gram
 
 def oneHotEncode(dim, idx):
@@ -47,7 +47,7 @@ class PositionalEncoding(nn.Module):
         
         
         
-    def set_device(self, dev):
+    def setDevice(self, dev):
         self.device = dev
     
         # self.register_buffer('pe', pe.unsqueeze(0))
@@ -74,20 +74,33 @@ class PositionalEncoding(nn.Module):
 # att = nn.MultiheadAttention(embed_dim= model_dim , num_heads= heads )
 
 class selfAttention(nn.Module):
-    def __init__(self , model_dim ,heads = 1):
+    def __init__(self , model_dim ,heads = 1 , device = torch.device("cpu")):
         super(selfAttention , self ).__init__()
         assert model_dim % heads == 0 , "O número de heads deve ser um divisor do número de dimensões do vetor de Embedding "
         self.heads    = heads 
         self.head_dim = int(model_dim / heads)
-        
-        self.keys    = nn.ModuleList([ nn.Linear(model_dim , self.head_dim , bias = False)  for i in range(heads)])
+
+        self.att = nn.MultiheadAttention(embed_dim= model_dim , num_heads= heads , device=device)
+        self.q = nn.Linear( model_dim , model_dim).to(device)
+        self.k = nn.Linear( model_dim , model_dim).to(device)
+        self.v = nn.Linear( model_dim , model_dim).to(device)
+        self.device = device
+
+        """self.keys    = nn.ModuleList([ nn.Linear(model_dim , self.head_dim , bias = False)  for i in range(heads)])
         self.queries = nn.ModuleList([ nn.Linear(model_dim , self.head_dim , bias = False)  for i in range(heads)])
         self.values  = nn.ModuleList([ nn.Linear(model_dim , self.head_dim , bias = False)  for i in range(heads)])
         
         self.v = Variable(torch.rand(heads , self.head_dim , dtype = float) , requires_grad = True).float()
-        self.u = Variable(torch.rand(heads , self.head_dim , dtype = float) , requires_grad = True).float()
-        self.output  = nn.Linear(model_dim , model_dim )
+        self.u = Variable(torch.rand(heads , self.head_dim , dtype = float) , requires_grad = True).float()"""
+        # self.output  = nn.Linear(model_dim , model_dim )
         print("chegou aqui")
+
+    def setDevice(self , device ) :
+        self.q.to(device)
+        self.k.to(device)
+        self.v.to(device)
+        self.att.to(device)
+        self.device = device
 
     def weights(self)->list :
         weights = [self.output ]
@@ -95,73 +108,28 @@ class selfAttention(nn.Module):
         return weights
     def forward(self,value ,key , query, scale = True , mask = False ) :
         
-        
 
-        keys    = torch.cat([k(key  ) for k in self.keys    ],dim=0)
-        queries = torch.cat([q(query) for q in self.queries ],dim=0)
-        values  = torch.cat([v(value) for v in self.values  ],dim=0)
+        k = self.k(key)
+        q = self.q(query)
+        v = self.v(value)
         
-        keys    = torch.transpose(keys.reshape(self.heads ,key.shape[0], self.head_dim ) ,1,2)
-        queries = queries.reshape(self.heads ,query.shape[0] , self.head_dim )
-        values  = values.reshape(self.heads ,value.shape[0] , self.head_dim )
-
-
-        #EMBEDDING Posicional Relativo :
-        pos = torch.arange(0 , -keys.shape[2] , -1).view(1,1,-1)
-        pos = torch.cat([pos +i for i in torch.arange( query.shape[0] )] , dim=1)#Faz somente a matriz t-j
-        R   = torch.zeros(self.head_dim , query.shape[0] , key.shape[0])
-        # print("pos.shape = {} \nR.shape = {}".format(pos.shape , R.shape))
-        #             t.sin(t.cat(tuple(pos*(1e4**(-(2*i)*(1/self.head_dim))) for i in t.arange(pe[0::2,:,:].shape[0])),dim = 0).float())
-        R[0::2,:,:] = torch.sin(torch.cat(tuple( pos*(1e4**(-2*i*(1/self.head_dim))) for i in torch.arange( R[0::2,:,:].shape[0] )) , dim = 0 ).float() )
-        R[1::2,:,:] = torch.cos(torch.cat(tuple( pos*(1e4**(-2*i*(1/self.head_dim))) for i in torch.arange( int(R.shape[0]/2)) ) , dim = 0).float() )
-        
-        
-
-        UxK = torch.cat([torch.cat([i for j in torch.arange(queries.shape[1])] , dim = 0)  for i in torch.einsum("hi,hik->hk",[self.u , keys])] , dim=0 )
-        UxK = UxK.reshape(self.heads,queries.shape[1] , keys.shape[2])
-        # print("v*R.shape = " , torch.einsum("hd,dtj->htj",[self.v,R]).shape )
-        # # print("v.shape = {} ".format(self.v.shape))
-        # print("R.shape = {} \nqueries.shape = {}".format(R.shape ,queries.shape )  )
-        # # print("keys.shape = {} ".format(keys.shape) )
-        # print("UxK.shape = " , UxK.shape )
-        # print("queries*keys.shape = ", torch.einsum("lij,ljk->lik",[queries,keys]).shape )
-        # print("queries*R = ", torch.einsum("hid,dij->hij",[queries,R]).shape )  #torch.einsum("pld,hlp->hld",[R,queries])
-        attention = torch.einsum("lij,ljk->lik",[queries,keys]) + torch.einsum("hid,dij->hij",[queries,R]) + UxK + torch.einsum("hd,dtj->htj",[self.v,R]) 
-        
-        if mask :
-            # attention = torch.einsum("lij,ljk->lik",[queries,keys])
-            mask = torch.tril(torch.ones(attention.shape))
-            if scale :
-                attention = F.softmax(attention.masked_fill(mask==0, float("-1e18"))*(self.head_dim**-.5),dim = 2)
-            else :
-                attention = F.softmax(attention.masked_fill(mask==0, float("-1e18")),dim = 2)
-        else :
-            if scale :
-                attention = F.softmax(attention*(self.head_dim**-.5),dim = 2)
-            else :
-                attention = F.softmax(attention , dim = 2)
-        attention = torch.einsum("lij,ljk->lik" , [attention,values])
-        attention = torch.cat( tuple(i for i in attention ) , 1 )
-
-        # a = a.reshape(heads ,a_dim ,phrase_len)
-        # a = t.transpose(a.reshape(heads ,phrase_len,a_dim) ,1,2)
-        # b = b.reshape(heads ,phrase_len , b_dim)
-        ## a = a.reshape(a_len,a_dim,1)
-        ## b = b.reshape(b_len,1 ,b_dim)
-        # print(t.einsum("lij,ljk->lik",[b,a]))
-        #attention = F.softmax(t.einsum("lij,ljk->lik",[q,k])*(self.head_dim**-.5).float(),dim = 2)
-        # attention = t.einsum("lij,ljk->lik",[attention,values])
-        # attention = t.cat(tuple(i for i in attention),1)
-        #return self.output(attention)
-        
-        return self.output(attention)
+        attention , _ = self.att(q , k , v)
+        return attention #self.output(attention)
+    
 class transformerBlock(nn.Module):# A LAYER_NORM AINDA NÃO ME CONVENCEU
     def __init__(self, model_dim , heads , forward_expansion = 4):
         super(transformerBlock , self ).__init__()
         self.attention   = selfAttention(model_dim ,heads= heads)
         self.layerNorm0  = nn.LayerNorm(model_dim )
         self.layerNorm1  = nn.LayerNorm(model_dim )
-        self.feedForward = nn.Sequential(nn.Linear(model_dim,model_dim * forward_expansion),nn.ELU(),nn.Linear(model_dim * forward_expansion ,model_dim))
+        self.feedForward = nn.Sequential(nn.Linear(model_dim,model_dim * forward_expansion) , nn.ELU() , nn.Linear(model_dim * forward_expansion ,model_dim))
+
+    def setDevice(self , device ):
+        self.attention.to(device)
+        self.layerNorm0.to(device)
+        self.layerNorm1.to(device)
+        self.feedForward[0].to(device)
+        self.feedForward[2].to(device)
 
     def weights(self)->list:
         weights = self.attention.weights()
@@ -178,11 +146,18 @@ class transformerBlock(nn.Module):# A LAYER_NORM AINDA NÃO ME CONVENCEU
 
 
 class decoderBlock(nn.Module):
-    def __init__(self ,model_dim ,heads ,forward_expansion = 4):
+    def __init__(self ,model_dim ,heads ,forward_expansion = 4 , device = torch.device("cpu")):
         super(decoderBlock,self).__init__()
         self.attention = selfAttention(model_dim , heads)
         self.norm = nn.LayerNorm(model_dim)
         self.transformerBlock = transformerBlock(model_dim,heads , forward_expansion = forward_expansion)
+        self.device = device
+
+    def setDevice(self,device):
+        self.attention.to(device)
+        self.norm.to(device)
+        self.transformerBlock.setDevice(device)
+        self.device = device
 
     def weights(self)->list:
         weights = self.attention.weights()
@@ -200,6 +175,7 @@ class decoder(nn.Module):
                  EOS = None ,
                  forward_expansion = 4 ):
         super(decoder,self).__init__()
+
         # self.embedding = word_Embedding
         self.device = device
         self.model_dim = model_dim
@@ -222,7 +198,8 @@ class decoder(nn.Module):
             self.BOS = -self.EOS"""
         
         self.pos_Encoder = PositionalEncoding(model_dim, device)
-
+    def set_Device(self , device : torch.device):
+        self.device = device
     def weights(self)->list:
         weights = [self.linear_Out]
         for i in self.layers :
@@ -264,7 +241,8 @@ class decoder(nn.Module):
         sequence = self.BOS
         idx = [ 0 ] #Ta ERRADO
         while sequence[-1] != self.EOS and sequence.shape[0]< max_lengh  :# Ta errado
-            buffer = sequence
+            buffer = self.pos_Encoder(sequence)
+
             for l in self.layers :
                 buffer = l(buffer , Enc_values , Enc_keys)
             buffer = F.softmax(self.linear_Out(buffer[-1]) , dim = 0 )
